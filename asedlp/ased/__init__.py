f6ca92a422c123389446e6a450511db503c78a8d
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2020-04-22 08:48:57 +0200

    add __init__.py

---
+++ asedlp/ased/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import warnings
+
+# Suppressing `FutureWarning` and `DeprecationWarning`
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
