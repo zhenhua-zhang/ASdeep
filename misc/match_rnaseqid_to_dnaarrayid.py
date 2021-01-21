26831bd03c34098a572c85cf4771a6653da15334
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2021-01-21 15:28:40 +0100

    update match_rnaseqid_to_dnaarrayid.py

--- misc/match_rnaseqid_to_dnaarrayid.py
+++ misc/match_rnaseqid_to_dnaarrayid.py
@@ -1,76 +1,47 @@
 #!/usr/bin/env python
 '''
-getids
+This is a utility script to match genotypes and RNA-seq results for BIOS
+project.
 '''
 import sys
 
-rec_order = [
-    "ids", "bios_id", "uuid", "biobank_id.x", "person_id", "pheno_id",
-    "biobank_gwas_id", "dna_id", "rna_id.x", "rna_note", "gonl_id",
-    "old_gonl_id", "cg_id", "in_rp3", "biobank_id.y", "rna_id.y", "run_id",
-    "type", "freeze", "qc", "raw", "past_filter"
-]
+rnaseqfile = sys.argv[1]
+with open(rnaseqfile, 'r') as fh:
+    rnaseqs = list({x.rsplit("/", 1)[-1].split("_")[0] for x in fh.readlines()})
 
-def mk_conv_dict(conv_rec, header_idx=0, sep='\t'):
-    header = conv_rec.pop(header_idx).strip().split(sep)
-    conv_dict = {}
-    for rec in conv_rec:
-        reclist = rec.strip().split(sep)
-        val = dict(zip(header, reclist))
-        if rec.startswith('NTR'):
-            key = 'NTR-' + reclist[0].split('-')[-1]
-        else:
-            key = reclist[0]
-
-        if key not in conv_dict:
-            conv_dict[key] = val
-        else:
-            print(key)
-            raise KeyError("Duplicated keys")
-
-    return conv_dict
+idmappingfile = sys.argv[2]
+with open(idmappingfile, 'r') as fh:
+    ids = [x.strip().split("\t") for x in fh.readlines()]
+    ids_dict = {dna_id: rna_id.split("_", 1)[0] for dna_id, rna_id in ids}
+    assert len(ids) == len(ids_dict)
 
 
-def mk_search_key(dnaid_rec, sep="\t"):
-    searchkeys = {}
-    for rec in dnaid_rec:
-        rec = rec.strip()
-        gntpid = rec.split(sep)[-1]
-        if rec.startswith("CODAM"):
-            key = rec.replace('\t', '-')
-        elif rec.startswith("NTR"):
-            key = "NTR-" + rec.split("F")[-1]
-        elif rec.startswith("PAN"):
-            key = "PAN-" + rec.split("_")[-1]
-        elif rec.startswith("RS"):
-            key = "RS-" + rec.split("_")[-1]
-        else:
-            raise ValueError("Unknown record: {}".format(rec))
+genotypes = {}
+genotypefile = sys.argv[3]
+with open(genotypefile, 'r') as fh:
+    for item in fh.readlines():
+        biobk_id, dna_id = item.strip().split("\t")
+        if biobk_id == "CODAM":
+            key = "-".join([biobk_id, dna_id.split("_", 1)[-1]])
+        elif biobk_id == "gonl":
+            key = dna_id
+        elif biobk_id == "LL":
+            key = "-".join([biobk_id, dna_id.split("_", 1)[-1]])
+        elif biobk_id in ["LLS_660Q", "LLS_OminExpr"]:
+            key = "-".join([biobk_id.split("_")[0], dna_id.split("_", 1)[-1]])
+        elif biobk_id == "NTR_Affy6":
+            key = "-".join([biobk_id.split("_")[0], dna_id.split("_", 1)[-1]])
+        elif biobk_id == "PAN":
+            key = "-".join([biobk_id, dna_id.split("_", 1)[-1]])
+        elif biobk_id == "RS":
+            key = "-".join([biobk_id, dna_id.split("_", 1)[-1]])
 
-        searchkeys[key] = gntpid
+        genotypes[key] = [dna_id, biobk_id]
 
-    return searchkeys
+match_ids = [[key, dna_id, ids_dict[key], bionk_id]
+             for key, (dna_id, bionk_id) in genotypes.items()
+             if ids_dict.get(key, "NULL") in rnaseqs]
 
-
-def mt_ids(cdict, skeys):
-    for key in skeys:
-        if key in cdict:
-            _convrec = cdict[key]
-            print(skeys[key] + "\t" + "\t".join(_convrec[x] for x in rec_order))
-        else:
-            print("Missing key-val pair: {}, {}".format(key, skeys[key]))
-
-
-if len(sys.argv) != 3:
-    sys.exit('Wrong number of arguments: require two, given {}'.format(len(sys.argv)))
-
-bios_id_conv_file = sys.argv[1]     # BIOS ID convert table
-bios_dnaarray_id_file = sys.argv[2] # BIOS genotype id
-
-with open(bios_id_conv_file) as convfh, open(bios_dnaarray_id_file, 'r') as dnafh:
-    convrec = convfh.readlines()
-    dna_array_rec = dnafh.readlines()
-
-    convdict = mk_conv_dict(convrec, sep=",")
-    searchkey = mk_search_key(dna_array_rec)
-    mt_ids(convdict, searchkey)
+print("#uniq_id\tdna_id\trna_id\tbiobank_id")
+for item in match_ids:
+    print("\t".join(item))
