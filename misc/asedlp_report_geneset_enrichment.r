72e0f6ac6da7f1abb563c8122267396c53829519
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2021-06-03 15:01:26 +0200

    update misc/

--- misc/asedlp_report_geneset_enrichment.r
+++ misc/asedlp_report_geneset_enrichment.r
@@ -1,4 +1,8 @@
 #!/usr/bin/env Rscript
+#
+## A utility script to check the enrichment of identified ASE genes
+#
+
 library(dplyr)
 library(ggplot2)
 library(enrichplot)
@@ -7,13 +11,22 @@ library(clusterProfiler)
 
 rm(list=ls())
 
-wkdir <- "/home/umcg-zzhang/Documents/projects/wp_ase_dlp/outputs/aseReports/geuvadis"
+min_ratio <- 0.1
+cohort <- "Geuvadis"
+
+wkdir <- paste("~/Documents/projects/wp_ase_dlp/outputs/aseReports", cohort, sep="/")
 setwd(wkdir)
 
-min_ratio <- 0.1
-cohort <- "geuvadis"
+#
+## Basic statistical analysis
+#
 
-file_path <- paste(wkdir, "asedlp_report_geuvadis-exMHC-pvm_sorted_filtered.csv", sep="/")
+
+#
+## Estimate
+#
+
+file_path <- paste(wkdir, "asedlp_report_exMHC-pvm_sorted_filtered_adj.csv", sep="/")
 pval_mtx <- read.csv(file_path, header=TRUE) %>%
     filter(ratio >= min_ratio) %>%
     as.data.frame()
@@ -22,61 +35,69 @@ cat("In total, there are ", nrow(pval_mtx),
     " genes with at least ", min_ratio * 100,
     "% individuals show allelic expression.\n", sep="")
 
-# GO terms
+
+#
+## GO terms
+#
 ont_type="BP"
-ergo <- enrichGO(gene=rownames(pval_mtx),
+ergo <- enrichGO(gene=as.character(pval_mtx$X),
                  OrgDb=org.Hs.eg.db,
+                 keyType="ENSEMBL",
                  ont=ont_type,
                  pAdjustMethod="BH",
                  pvalueCutoff=0.01,
-                 qvalueCutoff = 0.05,
-                 readable = TRUE)
+                 qvalueCutoff=0.05,
+                 readable=TRUE)
 
 show_cat <- 20
 h <- show_cat / 4 + 1
 w <- max(nchar(head(ergo@result$Description, show_cat))) / 15 + show_cat / 3 + 1
-
-ggsave(paste(cohort, ont_type, min_ratio, "dotplot.pdf", sep="-"),
+ggsave(paste(ont_type, min_ratio, "dotplot.pdf", sep="-"),
        plot=enrichplot::dotplot(ergo, showCategory=show_cat),
        width=w,
        height=h)
 
-ggsave(paste(cohort, ont_type, min_ratio, "emapplot.pdf", sep="-"),
+ggsave(paste(ont_type, min_ratio, "emapplot.pdf", sep="-"),
        plot=enrichplot::emapplot(ergo, showCategory=show_cat),
        width=10,
        height=10)
 
-ggsave(paste(cohort, ont_type, min_ratio, "upsetplot.pdf", sep="-"),
+ggsave(paste(ont_type, min_ratio, "upsetplot.pdf", sep="-"),
        plot=enrichplot::upsetplot(ergo, n=show_cat),
        width=w,
        height=h * 2)
 
-# KEGG pathways
-ont_type <- 'KEGGpathway'
-erkegg <- enrichKEGG(gene=rownames(pval_mtx),
-                     organism = "hsa", 
-                     keyType = "kegg", 
-                     pvalueCutoff = 0.05, 
-                     pAdjustMethod = "BH", 
-                     minGSSize = 10, 
-                     maxGSSize = 500, 
-                     qvalueCutoff = 0.2, 
-                     use_internal_data = FALSE)
 
-h <- show_cat / 4 + 1
-w <- max(nchar(head(erkegg@result$Description, show_cat))) / 15 + show_cat / 3 + 1
-ggsave(paste(cohort, ont_type, min_ratio, "dotplot.pdf", sep="-"),
-       plot=dotplot(erkegg, showCategory=show_cat),
-       width=w,
-       height=h)
+# 
+## KEGG pathways
+#
+if (FALSE) {
+    ont_type <- 'KEGGpathway'
+    erkegg <- enrichKEGG(gene=pval_mtx$X,
+                         organism = "hsa", 
+                         keyType="ENSEMBL",
+                         pvalueCutoff = 0.05, 
+                         pAdjustMethod = "BH", 
+                         minGSSize = 10, 
+                         maxGSSize = 500, 
+                         qvalueCutoff = 0.2, 
+                         use_internal_data = FALSE)
 
-ggsave(paste(cohort, ont_type, min_ratio, "emapplot.pdf", sep="-"),
-       plot=emapplot(erkegg, showCategory=show_cat),
-       width=10,
-       height=10)
+    h <- show_cat / 4 + 1
+    w <- max(nchar(head(erkegg@result$Description, show_cat))) / 15 + show_cat / 3 + 1
+    ggsave(paste(ont_type, min_ratio, "dotplot.pdf", sep="-"),
+           plot=dotplot(erkegg, showCategory=show_cat),
+           width=w,
+           height=h)
 
-ggsave(paste(cohort, ont_type, min_ratio, "upsetplot.pdf", sep="-"),
-       plot=upsetplot(erkegg, n=show_cat),
-       width=w,
-       height=h * 2)
+    ggsave(paste(ont_type, min_ratio, "emapplot.pdf", sep="-"),
+           plot=emapplot(erkegg, showCategory=show_cat),
+           width=10,
+           height=10)
 
+    ggsave(paste(ont_type, min_ratio, "upsetplot.pdf", sep="-"),
+           plot=upsetplot(erkegg, n=show_cat),
+           width=w,
+           height=h * 2)
+
+}
--- misc/prepare_bios_vars.sh
+++ misc/prepare_bios_vars.sh
@@ -8,58 +8,35 @@
 # License   : MIT
 #
 
+# TODO: Recover to the first version.
+
 if [[ -e /apps/modules/modules.bashrc ]]; then
     source /apps/modules/modules.bashrc
 fi
 
 set -Eeu -o pipefail
 
-# Cohorts: CODAM  NTR  PAN  RS LL
-# Sample information
-cohort="${1:?Error: You should give the cohort id!}"
-
-# Sub dirs
-#pjdir=/Documents/projects/wp_ase_dlp
-pjdir=/groups/umcg-bios/tmp01/users/umcg-zzhang/projects/wp_ase_dlp
-if [[ ! -d $pjdir/inputs/BIOS_genotypes/$cohort ]]; then
-    echo "Failed to find $pjdir/inputs/BIOS_genotypes/$cohort! Exit"
-    exit
-fi
-
-bcftools_dir=$pjdir/outputs/phasing_v2/$cohort/bcftools
-conformGt_dir=$pjdir/outputs/phasing_v2/$cohort/conformGt
-beagle_dir=$pjdir/outputs/phasing_v2/$cohort/beagle
-log_dir=$pjdir/outputs/phasing_v2/$cohort/logs
-
-mkdir -p $bcftools_dir $conformGt_dir $beagle_dir $log_dir
-
 debug=true
 if [[ $debug == true ]]; then
     runCpus=2
-    runMems=15G
-    runTime=0:9:0
-    runArray=1-1
-    set_flags=-Eeux
+    runMems=4G
+    runTime=0:59:0
+    runArray=22-22
 else
     runCpus=2
     runMems=15G
     runTime=19:59:0
     runArray=1-22
-    set_flags=-Eeu
 fi
-
-# Beagle settings
-seed=31415
-
 
 sbatch \
     --mem=$runMems \
     --time=$runTime \
     --array=$runArray \
     --cpus-per-task=$runCpus \
-    --job-name=phasing-$cohort \
-    --output=$log_dir/%A-%a_%u_phasing_$cohort.log \
-    <<EOF | cut -f4 -d' '
+    --job-name=phasing \
+    --output=%A-%a_%u_phasing.log \
+    <<"EOF" | cut -f4 -d' '
 #!/bin/bash
 
 # Ensure Slurm settings is updated.
@@ -67,74 +44,130 @@ if [[ -e /apps/modules/modules.bashrc ]]; then
     source /apps/modules/modules.bashrc
 fi
 
-set $set_flags -o pipefail
+# Set some shell attributes
+set -Eeu -o pipefail
 
-chr_id=\${SLURM_ARRAY_TASK_ID:=1}
+# Project (working) dirs
+pjdir=/groups/umcg-bios/tmp01/users/umcg-zzhang/projects/wp_ase_dlp
 
+# Version of phasing
+phasing_version=phasing_v2
+
+# Working on per chrommsome by jobarray of SLURM
+chrid=${SLURM_ARRAY_TASK_ID:-22}
+
+# Threads environmental variable from SLURM
+# Multiple CPUs per task, but --cpus-per-task is required to use the SLURM_CPUS_PER_TASK
+# variable
+threads=${SLURM_CPUS_PER_TASK:-$(grep -c processor /proc/cpuinfo)}
+
+# Files for each step
+bcftools_dir=$pjdir/outputs/$phasing_version/$chrid/bcftools
+conformGt_dir=$pjdir/outputs/$phasing_version/$chrid/conformGt
+beagle_dir=$pjdir/outputs/$phasing_version/$chrid/beagle
+
+# Create buffer directories
+mkdir -p $bcftools_dir $conformGt_dir $beagle_dir
+
+# BCFtools to filter the variants
 module purge && module load BCFtools/1.9-foss-2018b && module list
 
 # Input: upstream
-# The imputed files are located /groups/umcg-bios/prm02/projects/HRC_imputation,
+# The imputed files are located (calculon): /groups/umcg-bios/prm02/projects/HRC_imputation,
 # which are imputed using Michigan Imputation Server.
-imputed_gntp=$pjdir/inputs/BIOS_genotypes/$cohort/chr\${chr_id}.dose.vcf.gz
+id_mapping_file=$pjdir/inputs/idmapping/BIOS-genotype-rnaseq-ids_with-LL_usable_20210105.txt
 
 # Output: downstream
-imputed_filtered_gntp=$bcftools_dir/\$(basename \${imputed_gntp/vcf.gz/filtered.vcf.gz})
+imputed_filtered_gntp=$bcftools_dir/bios-${chrid}.filtered.merged.vcf.gz
 
-# R2 refer to Xiaojing's QC on 200HIV genotypes (>=0.3);
-# MAF is much smaller than the one used in GWAS analysis (>=0.1)
-bcftools view \
-    --include 'TYPE=="snp" && INFO/MAF >= 0.0005 && INFO/R2 >= 0.3' \
-    --threads $runCpus \
-    --output-type z \
-    --output-file \$imputed_filtered_gntp \
-    \$imputed_gntp
+for vcffile in $pjdir/inputs/BIOS_genotypes/*/chr${chrid}.dose.vcf.gz; do
+    cohort=$(basename $(dirname $vcffile))
+    vcffile_bnm=$cohort-$(basename $vcffile)
 
+    # Rename samples: using RNA-seq id instead of genotyping id
+    vcffile_rh=$bcftools_dir/${vcffile_bnm/.vcf.gz/.rh.vcf.gz}
+    bcftools reheader \
+        --samples <(cut -f2,3 $id_mapping_file) \
+        --output $vcffile_rh \
+        $vcffile
+
+    bcftools index \
+        --threads $threads \
+        --force \
+        --tbi \
+        $vcffile_rh
+
+    # Select only samples in id mapping file, i.e., samples with both RNA-seq and genotypes
+    # R2 refer to Xiaojing's QC on 200HIV genotypes (>=0.3);
+    vcffile_rh_ni=$bcftools_dir/${vcffile_bnm/.vcf.gz/.rh.ni.vcf.gz}
+    bcftools view \
+        --force-samples \
+        --samples-file <(cut -f3 $id_mapping_file) \
+        --output-type z \
+        --output-file $vcffile_rh_ni \
+        --threads $threads \
+        $vcffile_rh
+        # --include 'INFO/R2 >= 0.3' \
+
+    # Index it as bcftools/merge requires index
+    bcftools index \
+        --threads $threads \
+        --force \
+        --tbi \
+        $vcffile_rh_ni
+
+    rm -f $vcffile_rh $vcffile_rh.tbi
+done
+
+# Merge all usable variants. 
+# NOTE: the INFO/MAF should be updated and INFO[R2,[ER2]] fields should be ignored
+# TODO: A way to allocate CPUs for the used subcmds including merge, annotate, plugin, view.
+bcftools merge \
+    --force-samples \
+    --threads $(( $threads - 3 <= 0 ? 0 : $threads - 3 )) \
+    $bcftools_dir/*.rh.ni.vcf.gz \
+    | bcftools annotate --remove INFO/MAF,INFO/R2,INFO/ER2 \
+    | bcftools plugin fill-tags -- -t MAF,AF \
+    | bcftools view \
+        --include 'TYPE=="snp" && INFO/MAF >= 0.005 && N_ALT == 1' \
+        --output-type z \
+        --output-file $imputed_filtered_gntp \
 
 # Beagle version 5.1 which is powered by Java 1.8 or higher.
 module purge && module load Java/1.8.0_144 && module list
 
 # Input: reference
-ref_panel_gntp=$pjdir/inputs/beagle/reference_panel/chr\${chr_id}.1kg.phase3.v5a.vcf.gz
+ref_panel_gntp=$pjdir/inputs/beagle/reference_panel/chr${chrid}.1kg.phase3.v5a.vcf.gz
 
 # Input: upstream
 # imputed_filtered_gntp
 
 # Output: downstream
-imputed_filtered_adj_gntp=$conformGt_dir/\$(basename \${imputed_filtered_gntp/vcf.gz/adjByRef})
+imputed_filtered_adj_gntp=$conformGt_dir/$(basename ${imputed_filtered_gntp/vcf.gz/adjByRef})
 
 java -jar ~/tools/beagle/conform-gt.24May16.cee.jar \
-    gt=\$imputed_filtered_gntp \
-    ref=\$ref_panel_gntp \
-    chrom=\$chr_id \
+    gt=$imputed_filtered_gntp \
+    ref=$ref_panel_gntp \
+    chrom=$chrid \
     match=POS \
-    out=\$imputed_filtered_adj_gntp
+    out=$imputed_filtered_adj_gntp
 
 # Phasing.
 # Input: reference map and genotype.
-ref_panel_map=$pjdir/inputs/beagle/genetic_maps/plink.chr\${chr_id}.GRCh37.map
+ref_panel_map=$pjdir/inputs/beagle/genetic_maps/plink.chr${chrid}.GRCh37.map
 
 # Output: final
-imputed_filtered_adj_phased_gntp=$beagle_dir/beagle-$cohort-chr\${chr_id}
-
-# TODO: A blacklist of samples without RNA-seq results.
+imputed_filtered_adj_phased_gntp=$beagle_dir/beagle-chr${chrid}
 
 java -jar ~/tools/beagle/beagle.18May20.d20.jar \
-    gt=\$imputed_filtered_adj_gntp".vcf.gz" \
-    ref=\$ref_panel_gntp \
-    map=\$ref_panel_map \
+    gt=$imputed_filtered_adj_gntp".vcf.gz" \
+    ref=$ref_panel_gntp \
+    map=$ref_panel_map \
     impute=false \
-    seed=$seed \
-    nthreads=$runCpus \
-    out=\$imputed_filtered_adj_phased_gntp
+    seed=31415 \
+    nthreads=$threads \
+    out=$imputed_filtered_adj_phased_gntp
 
-bcftools view \
-    --include 'TYPE=="snp" && INFO/MAF>=0.0005 && INFO/R2>=0.3 && N_ALT==1' \
-    --threads $runCpus \
-    --output-type z \
-    --output-file \$imputed_filtered_adj_phased_gntp \
-    \${imputed_filtered_adj_phased_gntp/.vcf.gz/-singlealt.vcf.gz}
-
-rm -f \$imputed_filtered_adj_phased_gntp
+rm -f $imputed_filtered_adj_phased_gntp
 EOF
 
