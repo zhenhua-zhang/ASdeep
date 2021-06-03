2ac598670205945f8020f62dd6ed071e9dd1a8b9
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2021-06-03 14:58:51 +0200

    add prepare_gtex_*

--- misc/prepare_gtex_ase.sh
+++ misc/prepare_gtex_ase.sh
@@ -1,18 +1,98 @@
 #!/bin/bash
+
+#
+## A script to execute prepare_gtex_ase.py which generates ASE count table for given tissue.
+#
+
+# By default, this script submit jobs to the cluster workload manager SLURM, but it's also
+# possible to run the script on local machine.
+
+set -Eeu -o pipefail
+
+# Workingspace
 projdir=~/Documents/projects/wp_ase_dlp
 iptdir=$projdir/inputs
-optdir=$projdir/inputs
+optdir=$projdir/outputs
+scpdir=$projdir/scripts
 
-ase_count_path=$iptdir/GTEx/ase/whlbld
-liftover_matrix=$iptdir/GTEx/misc/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz
+# Inputs
+tissue=WHLBLD
+ase_table_path=$iptdir/GTEx/ase
+lookup_table=$iptdir/GTEx/misc/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.snp.only_id.tsv.gz
 
-for ase_table in $ase_count_path/*.ase_table.tsv.gz; do
-    echo ${ase_table}
-    csvtk join -d$'\t' -f'VARIANT_ID;variant_id' $ase_table $liftover_matrix \
-        | grep -ie TISSUE_ID -e WHLBLD \
-        | csvtk cut -I -f31,30,9,10,11 \
-        | csvtk sep -I -f1 -s'_' -n'config,position,refAllele,altAllele' --drop \
-        | csvtk cut -I -f6,7,2,8,9,3,4,5 \
-        | csvtk rename -I -f3,6,7,8 -n variantID,refCount,altCount,totalCount \
-        > ${ase_table/.tsv.gz/.whlbld.csv}
+# Output
+mkdir -p $optdir/aseQuan_v2/GTEx/$tissue
+
+# local machine or cluster?
+use_cluster=true
+if [[ $use_cluster == true ]]; then
+    # Resource
+    reqmem=12G
+    reqcpus=2
+    reqtime=09:59:00
+    jobname=prepare_gtex_ase
+    logfile=$optdir/aseQuan_v2/GTEx/$tissue/%j-%u-prepare_gtex_ase.log
+
+    execmd="sbatch \
+        --time $reqtime \
+        --mem-per-cpu $reqmem \
+        --cpus-per-task $reqcpus \
+        --output $logfile \
+        --job-name $jobname"
+else
+    execmd='bash'
+    reqcpus=2
+fi
+
+# Worker
+$execmd <<EOF
+#!/bin/bash
+set -u -o pipefail
+
+module load Python/3.7.4-GCCcore-8.3.0
+source $scpdir/.env/bin/activate
+
+# Determine the number of CPUs
+threads=\${SLURM_CPUS_PER_TASK:-$reqcpus}
+
+n_done_jobs=-\$threads
+for ase_table in $ase_table_path/*.tsv.gz; do
+    input_file=\$(basename \$ase_table)
+    output_file=\${input_file/.tsv.gz/.$tissue.csv}
+
+    # Skip current input file if the correponding output was found
+    if [[ -f $optdir/aseQuan_v2/GTEx/$tissue/\$output_file ]]; then
+        echo "[W]: Output file found, skip input file \$input_file"
+        continue
+    fi
+
+    # If no given tissue was found, skip the ASE count table.
+    has_tissue=\$(zgrep -wcm 1 $tissue \$ase_table)
+    if [[ \$has_tissue -gt 0 ]]; then
+
+        echo "[I]: Process input file \$input_file"
+        python $scpdir/misc/prepare_gtex_ase.py \
+            -l $lookup_table \
+            -a \$ase_table \
+            -t $tissue \
+            -o $optdir/aseQuan_v2/GTEx/$tissue/\$output_file &
+
+        n_done_jobs=\$(( n_done_jobs + 1 ))
+    else
+        echo "[W]: No record of $tissue was found in \$input_file."
+    fi
+
+    # Using the resource as efficient as possible.
+    n_running_jobs=\$(jobs | grep -c Running)
+    while [[ \$n_running_jobs -ge \$threads ]]; do
+        echo -ne "[I]: \$n_running_jobs job(s) running. \$n_done_jobs job(s) done.\r"
+        sleep 5s
+        n_running_jobs=\$(jobs | grep -c Running)
+    done
 done
+
+# Guard last background jobs.
+if [[ \$n_running_jobs -gt 0 ]]; then wait; fi
+
+echo "[I]: Totally, \$n_done_jobs job(s) were done."
+EOF
--- misc/prepare_gtex_vars.sh
+++ misc/prepare_gtex_vars.sh
@@ -7,67 +7,122 @@
 ## A utility script to preprocessing variats data for GTEx cohort
 #
 
+# Update the resource parameters.
+reqmem=32G
+reqcpus=1
+reqtime=7:59:59
+
+sbatch \
+    --mem $reqmem \
+    --time $reqtime \
+    --cpus-per-task $reqcpus \
+    --job-name filter-gtex-vars \
+    --output %j-filter_gtex_vars.log \
+    <<'EOF'
+#!/bin/bash
+set -Ee -o pipefail
+
+#
+## Project directories, computational resource, input files
+#
 # Project directories
 projdir=~/Documents/projects/wp_ase_dlp
 iptdir=$projdir/inputs
 optdir=$projdir/outputs
 
-# Filter variants
+# Computational resource
+threads=${SLURM_CPUS_PER_TASK:-$(grep -c processor /proc/cpuinfo)}
+
+# Raw variants from GTEx
 raw_vars=$iptdir/GTEx/vcf/phASER_WASP_GTEx_v8_merged.vcf.gz
-clean_vars=${raw_vars/.vcf.gz/.clean.nochr.vcf}
 
-# Liftover
-clean_vars_b37=${clean_vars/.vcf.gz/.b37.vcf}
-ref_genome_b37=$iptdir/GRCh37_reference/human_g1k_v37.fasta
-convert_chain=$iptdir/Ensembl_references/GRCh38_to_GRCh37.chain.gz
 
-# Update the resource parameters.
-stepmem=1G
-stepcpus=5
-steptime=2:59:59
+#
+## Generate index
+#
+module purge && module load BCFtools/1.9-foss-2018a && module list
 
-sbatch \
-    --mem $stepmem \
-    --time $steptime \
-    --cpus-per-task $stepcpus \
-    --job-name filter-gtex-vars \
-    --output %j-filter_gtex_vars.log \
-    <<EOF
-#!/bin/bash
-set -Ee -o pipefail
-module load BCFtools/1.9-foss-2018a
+if [[ ! -f $raw_vars.tbi ]]; then
+    bcftools index --threads $threads --tbi $threads $raw_vars
+else
+    echo Find index for $raw_vars
+fi
 
-# Generate index
-bcftools index \
-    --threads $stepcpus $raw_vars
 
-# Filter variants
-bcftools view 
-    --threads $stepcpus \
+#
+## Filter variants. hq = high quality; nc = no chr
+#
+# Filter variants. Only biallelic SNPs with AF from 0.005 to 0.995 and only 1 alternative allele
+chosen_hq_vars=$optdir/phasing/gtex/$(sed 's/.vcf.gz/_hq.vcf/g' <<<$(basename $raw_vars))
+chr_mapping_file=$iptdir/GTEx/vcf/phASER_WASP_GTEx_v8_merged.chr.txt
+
+bcftools view \
     -m 2 \
     -M 2 \
-    -v snps \
-    -i 'INFO/AF > 0.005 && INFO/AF <= 0.995' \
-    $raw_vars $(echo chr{1..22} | tr ' ' ',') \
-    | bcftools annotate --rename-chrs $chr_mapping_file \
-    >| ${clean_vars}
+    -i 'TYPE == "snp" && INFO/AF > 0.005 && INFO/AF <= 0.995 && N_ALT == 1' \
+    --threads $(( $threads / 2 )) \
+    $raw_vars $(echo chr{1..22} | tr " " ",") \
+    | bcftools annotate \
+        -O v \
+        -o $chosen_hq_vars \
+        --threads $(( $threads / 2 )) \
+        --rename-chrs $chr_mapping_file
 
-# Liftover from GRCh38 to GRCh37
-module purge
-module load Python/3.7.4-GCCcore-8.3.0
-source $projdir/script/.env/bin/activate
-CrossMap.py vcf $convert_chain $clean_vars $ref_genome_b37 $clean_vars_b37 --no-comp-alleles
 
-# Compress VCF
-module purge
-module load bcftools/1.9-foss-2018a
-bgzip -f -i -@ $stepcpus $clean_vars_b37
-bcftools index -@ $stepcpus -t $clean_vars_b37
+#
+## Liftover from GRCh38 to GRCh37
+#
+module purge && module load picard && module list
 
-# Clean up
-rm -f ${clean_vars}
+# Inputs and outputs for picard/LiftoverVcf
+chosen_hq_nc_b37_vars=${chosen_hq_vars/.vcf/_b37.vcf}
+reject_vars=${chosen_hq_vars/.vcf/_rej.vcf}
+ref_genome_b37=$iptdir/GRCh37_reference/human_g1k_v37.fasta
+convert_chain=$iptdir/UCSC_browser/hg38ToHg19.over.nochr.chain.gz
 
-# Report
+java -jar $EBROOTPICARD/picard.jar LiftoverVcf \
+    -I $chosen_hq_vars \
+    -O $chosen_hq_nc_b37_vars \
+    -R $ref_genome_b37 \
+    -CHAIN $convert_chain \
+    -REJECT $reject_vars \
+    -WARN_ON_MISSING_CONTIG true \
+    --TMP_DIR $optdir/phasing/gtex
+
+
+#
+## Annotate VCF
+#
+module purge && module load BCFtools/1.9-foss-2018a && module list
+
+# Input and output
+ref_vars=$iptdir/dbSNP/00-All.vcf.gz
+chosen_hq_nc_b37_annot_vars=${chosen_hq_nc_b37_vars}_annot.vcf.gz
+
+# Compress successfully liftovered varaints and generate index
+bcftools annotate \
+    -O z \
+    -c ID \
+    -a $ref_vars \
+    --threads $threads \
+    -o $chosen_hq_nc_b37_annot_vars \
+    $chosen_hq_nc_b37_vars
+
+bcftools index --threads $threads --tbi $chosen_hq_nc_b37_annot_vars
+
+# Compress rejected variants
+bcftools view \
+    --threads $threads \
+    --output-type z \
+    --output-file $reject_vars.gz \
+    $reject_vars
+
+
+#
+## Clean up
+#
+rm -f $chosen_hq_vars $chosen_hq_nc_b37_vars $chosen_hq_nc_b37_vars.idx $reject_vars
 echo Done
+
 EOF
 
