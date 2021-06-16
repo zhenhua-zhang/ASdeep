ffef0a164cba5b8b2687f40ae3479e742f117032
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2021-06-17 01:04:22 +0200

    using double quote instead

--- src/asedeep/train.py
+++ src/asedeep/train.py
@@ -6,7 +6,7 @@
 # License     : MIT
 # Created date: Thu 12 Mar 2020 05:44:52 PM CET
 
-'''A module to train a convolutionary neural network.'''
+"""A module to train a convolutionary neural network."""
 
 import sys
 import logging
@@ -33,7 +33,7 @@ from .dataset import ASEDataset, SeqToHilbertAndMakeLabel, MultipleTestAdjustmen
 
 
 class Trainer:
-    '''A class to train the neuroal network.'''
+    """A class to train the neuroal network."""
     def __init__(self, net, gene_id=None, file_path_pool=None, logging_path=None,
                  log_per_n_epoch=5, num_workers=12):
         self.net = net                         # NN model will be used
@@ -52,7 +52,7 @@ class Trainer:
 
     @staticmethod
     def _check_device():
-        return 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        return "cuda:0" if torch.cuda.is_available() else "cpu"
 
     @staticmethod
     def _eval_metric(true_label, pred_label, pred_score, class_label=[0, 1, 2], percent=True):
@@ -73,12 +73,12 @@ class Trainer:
             onehot_true_labels[item_idx][class_idx] = 1
 
         try:
-            rcac = roc_auc_score(onehot_true_labels, pred_score, average='macro', multi_class='ovr') * scale
+            rcac = roc_auc_score(onehot_true_labels, pred_score, average="macro", multi_class="ovr") * scale
         except ValueError as e:
             rcac = 0
 
-        prec = precision_score(true_label, pred_label, average='macro', zero_division=1) * scale
-        rcll = recall_score(true_label, pred_label, average='macro', zero_division=1) * scale
+        prec = precision_score(true_label, pred_label, average="macro", zero_division=1) * scale
+        rcll = recall_score(true_label, pred_label, average="macro", zero_division=1) * scale
         accu = accuracy_score(true_label, pred_label) * scale
 
         return fpr_list, tpr_list, rcac, prec, accu, rcll
@@ -88,11 +88,11 @@ class Trainer:
             if model_state is not None:
                 self.net.load_state_dict(torch.load(model_state))
             else:
-                logging.error('The model has NOT trained, require model states')
+                logging.error("The model has NOT trained, require model states")
                 sys.exit()
 
         if testloader is None:
-            logging.error('Missing testloader!! Exit...')
+            logging.error("Missing testloader!! Exit...")
             return None, None, None, None
 
         if criterion is None:
@@ -104,7 +104,7 @@ class Trainer:
         test_loss = 0.0
         with torch.no_grad(): # The model should be trained.
             for data in testloader:
-                inputs_type = torch.FloatTensor if device == 'cpu' else torch.cuda.FloatTensor
+                inputs_type = torch.FloatTensor if device == "cpu" else torch.cuda.FloatTensor
 
                 inputs = data[0].type(inputs_type).to(device)
                 true_label = data[1][1].to(device)
@@ -112,13 +112,13 @@ class Trainer:
                 outputs = self.net(inputs)
 
                 _, pred_label = torch.max(outputs.data, 1)
-                pred_label_list.extend(pred_label.to('cpu'))
-                true_label_list.extend(true_label.to('cpu'))
+                pred_label_list.extend(pred_label.to("cpu"))
+                true_label_list.extend(true_label.to("cpu"))
                 
                 if pred_score_list is None:
-                    pred_score_list = outputs.data.to('cpu')
+                    pred_score_list = outputs.data.to("cpu")
                 else:
-                    pred_score_list = torch.cat((pred_score_list, outputs.data.to('cpu')))
+                    pred_score_list = torch.cat((pred_score_list, outputs.data.to("cpu")))
 
                 test_loss += criterion(outputs, true_label).item()
 
@@ -132,12 +132,12 @@ class Trainer:
             for idx, x in enumerate(fpr):
                 if np.isnan(x):
                     x = 0
-                self.writer.add_scalars('ROC_AUC_curve/{}'.format(tag), {str(class_idx): tpr[idx]}, x)
+                self.writer.add_scalars("ROC_AUC_curve/{}".format(tag), {str(class_idx): tpr[idx]}, x)
 
     def _train(self, eps, optimizer, device, criterion, splits, batch_size, shuffle,
                log_per_n_epoch=5):
         # The exact implementation of train.
-        logging.info('Action, Epoch,  Accuracy, Precision,    Recall,    ROCAUC,      Loss')
+        logging.info("Action, Epoch,  Accuracy, Precision,    Recall,    ROCAUC,      Loss")
         train_idx, test_idx = splits
         trainloader = DataLoader(Subset(self.dataset, train_idx), batch_size=batch_size,
                                  shuffle=shuffle, num_workers=self.num_workers)
@@ -146,7 +146,7 @@ class Trainer:
 
         n_batch = 1
         epoch_loss_list = []
-        log_string = '{: >6}, {: >5n}, {: >9.2f}, {: >9.2f}, {: >9.2f}, {: >9.2f}, {: >9.4f}'
+        log_string = "{: >6}, {: >5n}, {: >9.2f}, {: >9.2f}, {: >9.2f}, {: >9.2f}, {: >9.4f}"
         tn_fpr, tn_tpr, tt_fpr, tt_tpr = None, None, None, None
         for epoch in range(eps):
             inputs = None
@@ -154,7 +154,7 @@ class Trainer:
             true_label_list, pred_label_list, pred_score_list = [], [], None
 
             for _, data in enumerate(trainloader):
-                inputs_type = torch.FloatTensor if device == 'cpu' else torch.cuda.FloatTensor
+                inputs_type = torch.FloatTensor if device == "cpu" else torch.cuda.FloatTensor
 
                 # Get inputs and true label ready
                 inputs = data[0].type(inputs_type).to(device)
@@ -173,13 +173,13 @@ class Trainer:
                 optimizer.step()      # Adjust model parameters
 
                 _, pred_label = torch.max(outputs.data, 1)
-                pred_label_list.extend(pred_label.to('cpu'))
-                true_label_list.extend(true_label.to('cpu'))
+                pred_label_list.extend(pred_label.to("cpu"))
+                true_label_list.extend(true_label.to("cpu"))
 
                 if pred_score_list is None:
-                    pred_score_list = outputs.data.to('cpu')
+                    pred_score_list = outputs.data.to("cpu")
                 else:
-                    pred_score_list = torch.cat((pred_score_list, outputs.data.to('cpu')))
+                    pred_score_list = torch.cat((pred_score_list, outputs.data.to("cpu")))
 
                 running_loss += train_loss.item()
 
@@ -192,11 +192,11 @@ class Trainer:
                 tn_fpr, tn_tpr, tn_roc_auc, tn_prec, tn_accu, tn_rcll = train_evm
 
                 # Write training evaluations to disk
-                self.writer.add_scalar('ROC_AUC_score/Train', tn_roc_auc, epoch)
-                self.writer.add_scalar('Precision/Train', tn_prec, epoch)
-                self.writer.add_scalar('Accuracy/Train', tn_accu, epoch)
-                self.writer.add_scalar('Recall/Train', tn_rcll, epoch)
-                self.writer.add_scalar('Loss/Train', running_loss, epoch)
+                self.writer.add_scalar("ROC_AUC_score/Train", tn_roc_auc, epoch)
+                self.writer.add_scalar("Precision/Train", tn_prec, epoch)
+                self.writer.add_scalar("Accuracy/Train", tn_accu, epoch)
+                self.writer.add_scalar("Recall/Train", tn_rcll, epoch)
+                self.writer.add_scalar("Loss/Train", running_loss, epoch)
                 logging.info(log_string.format("Train", epoch+1, tn_accu, tn_prec, tn_rcll,
                                                tn_roc_auc, running_loss))
 
@@ -209,34 +209,34 @@ class Trainer:
                 tt_fpr, tt_tpr, tt_roc_auc, tt_prec, tt_accu, tt_rcll  = test_evm
 
                 # Write test evaluations to disk
-                self.writer.add_scalar('ROC_AUC_score/Test', tt_roc_auc, epoch)
-                self.writer.add_scalar('Precision/Test', tt_prec, epoch)
-                self.writer.add_scalar('Accuracy/Test', tt_accu, epoch)
-                self.writer.add_scalar('Recall/Test', tt_rcll, epoch)
-                self.writer.add_scalar('Loss/Test', tt_loss, epoch)
-                logging.info(log_string.format('Test', epoch+1, tt_accu, tt_prec, tt_rcll,
+                self.writer.add_scalar("ROC_AUC_score/Test", tt_roc_auc, epoch)
+                self.writer.add_scalar("Precision/Test", tt_prec, epoch)
+                self.writer.add_scalar("Accuracy/Test", tt_accu, epoch)
+                self.writer.add_scalar("Recall/Test", tt_rcll, epoch)
+                self.writer.add_scalar("Loss/Test", tt_loss, epoch)
+                logging.info(log_string.format("Test", epoch+1, tt_accu, tt_prec, tt_rcll,
                                                tt_roc_auc, tt_loss))
 
             epoch_loss_list.append(running_loss)
 
-        self._add_roc_curve(tn_fpr, tn_tpr, 'Train')
-        self._add_roc_curve(tt_fpr, tt_tpr, 'Test')
+        self._add_roc_curve(tn_fpr, tn_tpr, "Train")
+        self._add_roc_curve(tt_fpr, tt_tpr, "Test")
         self.train_loss_list.append(epoch_loss_list)
 
     def init(self):
-        '''Init.'''
+        """Init."""
         # Check GPU or CPU
         self.writer = SummaryWriter(self.logging_path)
         self.net.to(self._check_device())
         return self
 
     def load_dataset(self, **kwargs):
-        '''Load the dataset for train or test.'''
+        """Load the dataset for train or test."""
         logging.info("Start to load ASEDataset...")
-        gene_id = kwargs.get('gene_id', self.gene_id)
-        file_path_pool = kwargs.get('file_path_pool', self.file_path_pool)
-        element_trans = kwargs.get('element_trans', SeqToHilbertAndMakeLabel())
-        dataset_trans = kwargs.get('dataset_trans', MultipleTestAdjustment())
+        gene_id = kwargs.get("gene_id", self.gene_id)
+        file_path_pool = kwargs.get("file_path_pool", self.file_path_pool)
+        element_trans = kwargs.get("element_trans", SeqToHilbertAndMakeLabel())
+        dataset_trans = kwargs.get("dataset_trans", MultipleTestAdjustment())
 
         if isinstance(gene_id, str):
             gene_id = [gene_id]
@@ -252,7 +252,7 @@ class Trainer:
         return self
 
     def train_test_split(self, train_size=0.7):
-        '''Split the dataset into train and test dataset.'''
+        """Split the dataset into train and test dataset."""
         labels = list(self.dataset.get_labels())
         label_idx = range(len(labels))
 
@@ -261,7 +261,7 @@ class Trainer:
         return self
 
     def train(self, eps=20, criterion=None, optimizer=None, learning_rate=1e-5, **kwargs):
-        '''Train the model.
+        """Train the model.
 
         Args:
             eps (int, optional, 20): Epoches.
@@ -269,19 +269,19 @@ class Trainer:
             optimizer (object, optional, None): Method to optimize the model.
             learning_rate (float, optional, 1e-5): Learning rate. Works when `optimizer` is None.
             **kwargs (Any, optinal): Other keyword arguments.
-        '''
+        """
         if criterion is None:
             criterion = nn.CrossEntropyLoss()
 
         if optimizer is None:
             optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
 
-        batch_size = kwargs.get('batch_size', 32)
-        shuffle = kwargs.get('shuffle', True)
+        batch_size = kwargs.get("batch_size", 32)
+        shuffle = kwargs.get("shuffle", True)
         device = self._check_device()
 
         if self.tt_splits is None:
-            logging.error('No train and test splits were found. Use train_test_split() first.')
+            logging.error("No train and test splits were found. Use train_test_split() first.")
             return self
 
         log_per_n_epoch = self.log_per_n_epoch
@@ -294,7 +294,7 @@ class Trainer:
         return self
 
     def loss_curve(self, loss_curve_path=None, **kwargs):
-        '''Save the loss curve.'''
+        """Save the loss curve."""
         train_loss_list = self.train_loss_list
         n_cv = len(train_loss_list)
 
@@ -304,42 +304,42 @@ class Trainer:
         elif n_cv > 1:
             loss_axe_pair = zip(train_loss_list, axes)
         else:
-            logging.error('The loss list is empty!!')
+            logging.error("The loss list is empty!!")
             return self
 
         for loss, ax in loss_axe_pair:
             epoch_list = list(range(len(loss)))
             ax.plot(epoch_list, loss)
-            ax.set_title('Loss per epoch')
-            ax.set_xlabel('Epoch')
-            ax.set_ylabel('Loss')
+            ax.set_title("Loss per epoch")
+            ax.set_xlabel("Epoch")
+            ax.set_ylabel("Loss")
 
         fig.savefig(loss_curve_path, **kwargs)
         logging.info("Check the loss curve at: {}".format(loss_curve_path))
 
         return self
 
-    def save_model(self, path, how='state'):
-        '''Save the trained model, typlically the state dictionary.'''
+    def save_model(self, path, how="state"):
+        """Save the trained model, typlically the state dictionary."""
         if self.net is None:
-            logging.error('The model is empty, have you ever run the train() method?')
+            logging.error("The model is empty, have you ever run the train() method?")
             sys.exit()
 
-        if how == 'state':
+        if how == "state":
             torch.save(self.net.state_dict(), path)
-        elif how == 'model':
+        elif how == "model":
             torch.save(self.net, path)
         else:
-            raise ValueError('Unsupported way to save the model: choose one from [state, model]')
+            raise ValueError("Unsupported way to save the model: choose one from [state, model]")
 
         return self
 
     def test(self, model_state: str = None, testloader: DataLoader = None):
-        '''Test for new dataset.'''
+        """Test for new dataset."""
         self._test(model_state, testloader)
 
         return self
 
 
-if __name__ == '__main__':
-    logging.error('This file is meant to be imported.')
+if __name__ == "__main__":
+    logging.error("This file is meant to be imported.")
